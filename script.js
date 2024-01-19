110a6dbea141663e89d3c435f5ff45603304953b
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2024-01-19 10:45:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function nextPalindrome(num) {
   //your JS code here.
+	parseInt(num);
+	num++;
+	while(true){
+		if(num.toString()==num.toString().split("").reverse().join("")){
+			return num;
+		}else{
+			parseInt(num);
+			num++
+		}
+	}
 }
 
 const input = prompt("Enter a palindrome number");
-alert(nextPalindrome(input));
+alert(nextPalindrome(input));{
+
+}
